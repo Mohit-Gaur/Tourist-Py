364be1624459262bb329709a246c6ca1c00a3a29
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-04-05 15:16:46 +0530

    Create tourist.py
    Basic starting code

---
+++ tourist.py
@@ -0,0 +1,27 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
+
+attractions = []
+for destination in destinations:
+  attractions.append([])
+print(attractions)
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index].append(attraction)
+    except ValueError:
+        return
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+print(attractions)
