72511c707a72223b3ee503e16d7d87a2fd1920a3
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-04-06 13:35:03 +0530

    Update tourist.py

--- tourist.py
+++ tourist.py
@@ -25,3 +25,56 @@ def add_attraction(destination, attraction):
 
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
 print(attractions)
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index].append(attraction)
+    except ValueError:
+        return
+
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attractions in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction)
+
+    return attractions_with_interest
+
+
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+    interests_string = "Hi " + traveler[0] + " , we think you'll like these places around " + traveler_destination
+
+    for i in range(len(traveler_attractions)):
+        if traveler_attractions[-1] == traveler_attractions[i]:
+            interests_string += "the " + traveler_attractions[i] + "."
+        else:
+            interests_string += "the " + traveler_attractions[i] + ", "
+    return interests_string
+
+
+smills_france = get_attractions_for_traveler(['Derek Smill', 'Paris, France', ['monument']])
+print(smills_france)
